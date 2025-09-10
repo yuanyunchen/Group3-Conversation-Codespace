86be96ef7ad4713860ab009d64f9ff4cf68516e5
kna2121 <155922868+kna2121@users.noreply.github.com>
2025-09-10 11:43:44 -0400

    Deliverable for 9/10 Group 7 (#11)
    * propose items in order of importance
    
    * Coherence potentially?
    
    * generate score based on importance and coherence
    
    * reformatted
    
    ---------
    
    Co-authored-by: Andrew Chung <achun46@emory.edu>

--- players/player_7/player.py
+++ players/player_7/player.py
@@ -6,4 +6,24 @@ class Player7(Player):
 		super().__init__(snapshot, conversation_length)
 
 	def propose_item(self, history: list[Item]) -> Item | None:
-		return None
+		current = None
+		max_score = 0
+
+		subject_count = {subject: 0 for subject in self.preferences}
+		for item in history[-3:]:
+			for subject in item.subjects:
+				subject_count[subject] += 1
+
+		for item in self.memory_bank:
+			if item in self.contributed_items:
+				continue
+			score = item.importance
+			for subject in item.subjects:
+				if subject_count[subject] > 0:
+					score += 1
+			if score > max_score:
+				max_score = score
+				current = item
+
+		self.contributed_items.append(current)
+		return current
