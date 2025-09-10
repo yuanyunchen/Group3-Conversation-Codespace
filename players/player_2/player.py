39d40aed4a66536216797987eef3d1dc44b1fd7e
ellenaj0 <83231427+ellenaj0@users.noreply.github.com>
2025-09-10 11:42:17 -0400

    Initial player 2 (#5)

--- players/player_2/player.py
+++ players/player_2/player.py
@@ -6,4 +6,13 @@ class Player2(Player):
 		super().__init__(snapshot, conversation_length)
 
 	def propose_item(self, history: list[Item]) -> Item | None:
-		return None
+		# Sort items by importance
+		self.memory_bank.sort(key=lambda i: i.importance)
+		# Check if all items used up
+		if len(self.memory_bank) == 0:
+			return None
+		# Pick most important item and propose it
+		last_item = self.memory_bank[-1]
+		if len(history) != 0 and history[-1] == last_item:
+			self.memory_bank.pop()
+		return self.memory_bank[-1]
